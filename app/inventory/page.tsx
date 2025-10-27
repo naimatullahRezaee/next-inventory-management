9ed5043c44f5b68943f527fb8e51683cdf5fcc30
naimatullahRezaee <naimatullahrezaee@gmail.com>
2025-10-27 10:33:18 +0430

    add searching...

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -4,10 +4,18 @@ import { getCurrentUser } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { Trash, Trash2 } from "lucide-react";
 
-export default async function InventoryPage() {
+export default async function InventoryPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const params = await searchParams;
+  const q = (params.q ?? "").trim();
   const user = await getCurrentUser();
   const userId = user.id;
-  const totalProducts = await prisma.product.findMany({ where: { userId } });
+  const totalProducts = await prisma.product.findMany({
+    where: { userId, name: { contains: q, mode: "insensitive" } },
+  });
   return (
     <div className="min-h-screen bg-gray-50">
       <Sidebar currentPath="/inventory" />
@@ -25,6 +33,19 @@ export default async function InventoryPage() {
           </div>
         </div>
         <div className="space-y-6">
+          {/* Search */}
+          <div className="bg-white rounded-lg border border-gray-200 p-6">
+            <form action="/inventory" className="flex gap-2" method="GET">
+              <input
+                name="q"
+                placeholder="Search products..."
+                className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:border-transparent "
+              />
+              <button className="px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700">
+                Search
+              </button>
+            </form>
+          </div>
           {/*  products table */}
           <div className="bg-white rounded-lg border border-gray-200 overflow-hidden">
             <table className="w-full">
