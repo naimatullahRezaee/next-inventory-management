346519d52bb18f342fc8f71c60b65e1252119b74
naimatullahRezaee <naimatullahrezaee@gmail.com>
2025-10-28 08:34:30 +0430

    add product page to create new product

--- lib/actions/products.ts
+++ lib/actions/products.ts
@@ -1,7 +1,17 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { getCurrentUser } from "../auth";
 import { prisma } from "../prisma";
+import { z } from "zod";
+
+const productSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  price: z.coerce.number().nonnegative("Price must be positive"),
+  quantity: z.coerce.number().int().min(0, "Quantity must be positive"),
+  sku: z.string().optional(),
+  losStock: z.coerce.number().int().min(0).optional(),
+});
 
 export async function deleteProduct(formData: FormData) {
   const user = await getCurrentUser();
@@ -9,4 +19,25 @@ export async function deleteProduct(formData: FormData) {
   await prisma.product.deleteMany({
     where: { id: id, userId: user.id },
   });
+}
+export async function createProduct(formData: FormData) {
+  const user = await getCurrentUser();
+  const parsed = productSchema.safeParse({
+    name: formData.get("name"),
+    price: formData.get("price"),
+    quantity: formData.get("quantity"),
+    sku: formData.get("sku") || undefined,
+    lowStockAt: formData.get("lowStockAt") || undefined,
+  });
+  if (!parsed.success) {
+    throw new Error("Validation failed");
+  }
+  try {
+    await prisma.product.create({
+      data: { ...parsed.data, userId: user.id },
+    });
+    redirect("/inventory");
+  } catch (error) {
+    throw new Error("Failed to create product.");
+  }
 }
